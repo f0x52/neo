e99231a79a839f0826703d72744e84b92110d7df
f0x <f@0x52.eu>
2018-05-03 21:43:20 +0200

    Fix webpack config to display correct svg

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]'
+              name: '[path][name].[ext]'
             }
           }
         ],
